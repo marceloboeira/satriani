242fa742e57146f341b2f94fccd940b54d4ba3c6
Marcelo Boeira <me@marceloboeira.com>
2016-05-28 22:36:54 -0300

    test: Add simple route scenario

--- spec/satriani_spec.cr
+++ spec/satriani_spec.cr
@@ -1,1 +1,17 @@
 require "./spec_helper"
+require "http/client"
+
+describe Satriani do
+  before do
+    Thread.new { Satriani::Application.new.start }
+  end
+
+
+  context "with a valid route" do
+    it "returns success" do
+      response = HTTP::Client.get("http://localhost:8000")
+
+      expect(response.status_code).to eq(200)
+    end
+  end
+end
