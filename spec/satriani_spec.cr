3d1a1a34374482eb4840f15ae97d694663c1f5c9
Marcelo Boeira <me@marceloboeira.com>
2016-07-01 23:45:37 -0300

    test: Add get request helper

--- spec/satriani_spec.cr
+++ spec/satriani_spec.cr
@@ -2,28 +2,34 @@ require "./spec_helper"
 require "http/client"
 
 describe Satriani do
-  let(routes) {
+  let(routes) do
     [
       Satriani::Route.new("/joe-satriani/always-with-me-always-with-you") {|request| "1987" },
       Satriani::Route.new("/joe-satriani/flying-in-a-blue-dream") {|request| "1989" },
     ]
-  }
+  end
 
-  let(application) { Satriani::Application.new(routes) }
+  let(application) do
+    Satriani::Application.new(routes)
+  end
 
   before do
     Thread.new { application.start }
   end
 
+  def get(path)
+    HTTP::Client.get("http://localhost:8000#{path}")
+  end
+
   context "with a valid route" do
     it "returns success" do
-      response = HTTP::Client.get("http://localhost:8000/joe-satriani/always-with-me-always-with-you")
+      response = get("/joe-satriani/always-with-me-always-with-you")
 
       expect(response.status_code).to eq(200)
     end
 
     it "renders the content" do
-      response = HTTP::Client.get("http://localhost:8000/joe-satriani/always-with-me-always-with-you")
+      response = get("/joe-satriani/always-with-me-always-with-you")
 
       expect(response.body).to eq("1987")
     end
@@ -31,7 +37,7 @@ describe Satriani do
 
   context "with a invalid route" do
     it "returns not found" do
-      response = HTTP::Client.get("http://localhost:8000/invalid")
+      response = get("/invalid")
 
       expect(response.status_code).to eq(404)
     end
